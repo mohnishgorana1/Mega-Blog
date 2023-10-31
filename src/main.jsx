7bfe75e0c8ea21ef616d5f14bbe16a3a4ec09db5
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-31 14:25:45 +0530

    routing done

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,75 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import store from "./store/store.js";
-import { createBrowserRouter } from "react-router-dom";
+
+import AuthLayout from "./components/AuthLayout.jsx";
+
+import {
+  Home,
+  AddPost,
+  AllPosts,
+  EditPosts,
+  Login,
+  Post,
+  Signup,
+} from "./pages";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:
-  }
-])
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {""}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {""}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store} >
-    
-    <App />
+  <Provider store={store}>
+    <RouterProvider router={router} />
   </Provider>
 );
