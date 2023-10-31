bca06be01f97d0d8613ec7d11e9053dd81847f27
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-31 14:19:06 +0530

    index of pages made

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import AddPost from './AddPost'
+import AllPosts from './AllPosts'
+import EditPosts from './EditPosts'
+import Home from './Home'
+import Login from './Login'
+import Signup from './Signup'
+import Post from './Post'
+
+export {
+    AddPost,
+    AllPosts,
+    EditPosts,
+    Home,
+    Login,
+    Signup,
+    Post
+}
+
+
